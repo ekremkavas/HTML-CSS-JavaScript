03d236c78b4838b9666ad3bee0b93ed2745bd67a
Ekrem <ekremkavas2012@gmail.com>
2024-01-15 21:48:29 +0300

    devam

---
+++ JavaScript-CLASS/0_java.js
@@ -0,0 +1,11 @@
+console.log("intro js içindeyiz");
+console.error("hata mesajı");
+console.warn("uyarı");
+
+//alert-uyarı vermek için
+
+// alert("lütfen adınızı girin");
+
+document.write("javascript intro");
+
+cevap = confirm("çıkmak istediğinize eminmisiniz");
