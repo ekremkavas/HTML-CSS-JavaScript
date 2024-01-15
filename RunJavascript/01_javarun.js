ff7ec8c1943af7ac1e2f973e8a65377728f2a7fd
Ekrem <ekremkavas2012@gmail.com>
2024-01-15 06:17:27 +0300

    devam

---
+++ RunJavascript/01_javarun.js
@@ -0,0 +1,24 @@
+// console.log("F1496-Ekrem");
+// document write ile çıktı verme
+
+// document.write("Hello Word <br>");
+// document.write("Hello Word");
+
+console.log("Hello Word");
+console.log(3.53);
+console.log(true);
+console.log({ name: "ekrem", surname: "cenkeri" });
+
+// console.clear();
+
+let a = 5;
+let b = 10;
+
+console.log("ikii sayının toplamı :", a + b);
+
+// alert ile uyarı verme
+// console.log(window);
+
+alert("F5 e basmayınız");
+
+console.log(window);
