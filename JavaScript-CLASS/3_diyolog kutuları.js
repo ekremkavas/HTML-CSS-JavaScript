2829472ce4611662a701ce44330044ec20e941e7
Ekrem <ekremkavas2012@gmail.com>
2024-01-18 06:48:31 +0300

    run

--- JavaScript-CLASS/3_diyolog kutuları.js
+++ JavaScript-CLASS/3_diyolog kutuları.js
@@ -2,11 +2,19 @@
 
 //? ALERT -PROMPT- CONFRIM
 
-alert("Hoşgeldiniz bu sizin alert mesajınız");
+//! alert("Hoşgeldiniz bu sizin alert mesajınız");
 
-// Prompt kullanıcıdan değer alabılırsın
-prompt("Kullıcı isminizi giriniz :");
+// Prompt kullanıcıdan değer alabılırsın , kullanıcıdan alınan değerler string döner
+//! prompt("Kullıcı isminizi giriniz :");
 
 // console yazdrımak ıcın
 let isim = prompt("ismnizi giriniz");
 console.log(isim);
+
+// Confrim onaylamak anlamındadır
+//! confirm("silmek istediğinizden emin misiniiz :");
+
+let sonuc = confirm("silmek istediğinizden emin misiniiz :");
+console.log(sonuc);
+
+// tamam a basarsanız true döner hayır a basmıssa false döner
---
+++ JavaScript-CLASS/4_if-else.js
@@ -0,0 +1,15 @@
+//KOŞUL YAPILARI
+
+let not = 65;
+if (not > 50) {
+  console.log("if bloğunun içi true çalıştı");
+} else {
+  console.log("if bloğu false else bloğu çalıştı");
+}
+
+let not2 = 35;
+if (not2 > 50) {
+  console.log("if bloğunun içi true çalıştı");
+} else {
+  console.log("if bloğu false else bloğu çalıştı");
+}
