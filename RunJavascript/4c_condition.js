68fefef5fe4c01009df8d5a84b694eb56e223226
ekrem <ekremkavas2012@gmail.com>
2024-01-20 13:15:57 +0300

    run

--- RunJavascript/4c_condition.js
+++ RunJavascript/4c_condition.js
@@ -5,5 +5,7 @@ console.log("***CONDİTİONS***");
 const num1 = Number(promt("bir sayı giriniz"));
 
 // prompt mutlaka string ifade sonuc verir
-const num2 = 5
+const num2 = 5;
+// + nın concatinate ve toplama görevi vardır. diğer matematik ifadelerinin tek görevi vardır
 console.log(num1, typeof num1);
+console.log("toplam :", num1 + num2);
