3965028822c756ad222e54c0c933f22db6ba6c49
ekrem <ekremkavas2012@gmail.com>
2024-01-20 11:59:20 +0300

    devam

---
+++ RunJavascript/4c_condition.js
@@ -0,0 +1,9 @@
+//KARAR YAPILARI
+
+console.log("***CONDİTİONS***");
+
+const num1 = Number(promt("bir sayı giriniz"));
+
+// prompt mutlaka string ifade sonuc verir
+const num2 = 5
+console.log(num1, typeof num1);
