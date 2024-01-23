ef5a9fbe68526eaa4eee83ab354d70c89d84bdf2
Ekrem <ekremkavas2012@gmail.com>
2024-01-23 05:26:07 +0300

    run

---
+++ RunJavascript/4d_ifelse.js
@@ -0,0 +1,14 @@
+let category = "milk";
+
+switch (category) {
+  case "coffee":
+    console.log("sade kahveniz hazırlanıyor");
+    break;
+
+  case "coffemilk":
+    console.log("sütlü kahveniz hazırlanıyor");
+    break;
+
+  default:
+    console.log("hatalı katogori seçtiniz");
+}
