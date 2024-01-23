4e510201d56dbd5df0b7b1ca867a0b8ea339297a
Ekrem <ekremkavas2012@gmail.com>
2024-01-23 06:46:04 +0300

    run

--- RunJavascript/4d_ifelse.js
+++ RunJavascript/4d_ifelse.js
@@ -1,14 +1,63 @@
-let category = "milk";
+console.log("swich-case başlıyoruz");
+//! 1.örnek
 
-switch (category) {
-  case "coffee":
-    console.log("sade kahveniz hazırlanıyor");
-    break;
+// let category = "milk";
 
-  case "coffemilk":
-    console.log("sütlü kahveniz hazırlanıyor");
-    break;
+// switch (category) {
+//   case "coffee":
+//     console.log("sade kahveniz hazırlanıyor");
+//     break;
 
-  default:
-    console.log("hatalı katogori seçtiniz");
+//   case "coffemilk":
+//     console.log("sütlü kahveniz hazırlanıyor");
+//     break;
+
+//   default:
+//     console.log("hatalı katogori seçtiniz");
+// }
+
+//! 2.örnek
+
+// let sayi = prompt("Lütfen 1 yada 2 arasında bir sayı giriniz");
+
+// switch(sayi) { //sayı seçimi
+//     case "1":
+//         console.log("girilen sayı 1'dir...");
+//         break;
+//      case "2":
+//         console.log("girilen sayı 2'dir...");
+//         break;
+
+//     default:
+//         console.log("girilen sayı istenilen değerde değildir");
+//             break;
+// }
+
+
+
+/* //? sorular
+1-Türkçe -4 
+2-matematik
+
+
+
+let turkceDogru, turkceYanlis = 0 ;
+let matematikDogru, matematikYanlis = 0;
+
+let yeniSatir ="\r\n" ;
+let mesaj = "Test sonucu uygulmasına hoşgeldiniz "
+ + yeniSatir 
+ + "1-paun hesapla " 
+ + "2-çıkış yap "
+
+//alert mesaj
+
+switch(secim){
+    case "1":
+        turkceDogruk =Number(prompt("türkçe doğru sayısı"));
+        turkceYanlis =Number(prompt("türkçe yalnis sayısı"));
+
+
 }
+
+*/
