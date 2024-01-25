53be864cb71c4a78caaa8468cd64bf799799b307
ekrem <ekremkavas2012@gmail.com>
2024-01-25 18:52:23 +0300

    run

--- RunJavascript/js04-Loops/04_forLoop.js
+++ RunJavascript/js04-Loops/04_forLoop.js
@@ -1,3 +1,23 @@
 //*********************************************
 //* FOOR LOOP
 //*********************************************
+
+// for(let i = 1; i <= 50; i++){
+//     console.log(i);
+
+// }
+
+/*
+for (başlangıç; koşul; artış/azalış) {
+     Döngü içinde yapılacak işlemler
+}
+*/
+
+for(let i = 1; i <= 50; i += 3){
+    console.log(i);
+
+}
+
+/* for döngülerinde koşul cümlelerinde == veya != operatörlerini kullanmak sonsuz veya girilmeyen döngüye yol açması muhtemeldir */
+
+
