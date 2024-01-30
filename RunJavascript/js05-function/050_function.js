730026abe1f8d996a70d6957b8e17a7f2e51d52a
ekrem <ekremkavas2012@gmail.com>
2024-01-30 15:21:08 +0300

    run

---
+++ RunJavascript/js05-function/050_function.js
@@ -0,0 +1,8 @@
+
+//1-Function Decleration ile Tanımlama'da fomksiyonun yerinin önemi yok nereye yazarsanız orda çağırır
+Cenkeri(); 
+function Cenkeri(){
+    console.log("Ben 5 yaşındayım");
+}
+//invoke (call)
+Cenkeri(); 
