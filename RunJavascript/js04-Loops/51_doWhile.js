97ef4bf6770fcab431a75ec6fecba629f46191c3
ekrem <ekremkavas2012@gmail.com>
2024-01-30 14:22:24 +0300

    run

--- RunJavascript/js04-Loops/51_doWhile.js
+++ RunJavascript/js04-Loops/51_doWhile.js
@@ -6,17 +6,29 @@
 
 // }while(sayac < 10);
 
+// ****************************************
 
+let sayac2 = 1;
 
-// let sayı =0;
+do {
+  if (sayac2 % 2 !== 0) {
+    console.log(sayac2);
+  }
+  sayac2++;
+} while (sayac2 <= 10);
 
-// do{
-//     console.log("1 den 20 e kadar tek sayılar");
-//     sayı++;
-    
-// }while(sayı<=20){
-//     if(sayı%2 == 1){
+// **************************************
 
-//     }
+//? Ornek
+let devam = "";
+do {
+  let vize = Number(prompt("Lütfen vize notunu giriniz:"));
+  let final = Number(prompt("Lütfen final notunu giriniz:"));
+  let ortalama = vize * 0.3 + final * 0.7;
 
-// }
+  const sonuc = ortalama >= 50 ? "Geçti" : "Kaldi";
+  console.log(sonuc);
+  
+  devam = prompt("Hesaplamaya devam etmek ister misiniz? e");
+} while (devam === "e" || devam === "E");
+console.log("Uygulama kapaniyor");
