b9c9be326d67bc872375f7193c47c936b67b6440
Ekrem <ekremkavas2012@gmail.com>
2024-01-30 06:46:39 +0300

    run

---
+++ RunJavascript/js04-Loops/50_while.js
@@ -0,0 +1,8 @@
+let sayac = 1;
+while (true) {
+    console.log(sayac);
+    if (sayac==7){
+        break; 
+    }
+    sayac++;
+}
---
+++ RunJavascript/js04-Loops/51_doWhile.js
@@ -0,0 +1,22 @@
+// let sayac =1;
+
+// do{
+//     console.log(sayac);
+//     sayac++;
+
+// }while(sayac < 10);
+
+
+
+// let sayı =0;
+
+// do{
+//     console.log("1 den 20 e kadar tek sayılar");
+//     sayı++;
+    
+// }while(sayı<=20){
+//     if(sayı%2 == 1){
+
+//     }
+
+// }
