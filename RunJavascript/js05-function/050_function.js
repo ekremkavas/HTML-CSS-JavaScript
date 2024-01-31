c7d02a862c3ee014a7b1e18ac87d943404f54137
Ekrem <ekremkavas2012@gmail.com>
2024-01-31 21:03:29 +0300

    run

--- RunJavascript/js05-function/050_function.js
+++ RunJavascript/js05-function/050_function.js
@@ -1,5 +1,12 @@
 
-//1-Function Decleration ile Tanımlama'da fomksiyonun yerinin önemi yok nereye yazarsanız orda çağırır
+//! 1-Function Decleration 
+
+// Function Declaration (Fonksiyon Deklarasyonu):ile Tanımlama'da fomksiyonun yerinin önemi yok nereye yazarsanız orda çağırır
+
+function myFunction() {
+    // Fonksiyonun işlemleri burada tanımlanır
+  }
+  
 
 function Cenkeri(){
     console.log("Ben 5 yaşındayım");
@@ -8,7 +15,28 @@ function Cenkeri(){
 Cenkeri(); 
 
 
-// 2-Parametreli Function
+function greet(name) {
+    console.log("Merhaba, " + name + "!");
+  }
+  
+  // Fonksiyonu çağırma
+  greet("Ahmet"); // Çıktı: Merhaba, Ahmet!
+
+  
+//! 2-Function Expression (Fonksiyon İfadesi): 
+
+var myFunction = function() {
+    // Fonksiyonun işlemleri burada tanımlanır
+  };
+  
+
+var greet = function(name) {
+    console.log("Merhaba, " + name + "!");
+  };
+  
+  // Fonksiyonu çağırma
+  greet("Ayşe"); // Çıktı: Merhaba, Ayşe!
+  
 
 function selamVer(mesaj){
     console.log(`Merhaba ${mesaj}`);
@@ -22,3 +50,28 @@ function topla(s1, s2){
 
 topla(7,5)
 
+//!3-Arrow Function (Ok Fonksiyonu):
+
+var myFunction = () => {
+    // Fonksiyonun işlemleri burada tanımlanır
+  };
+  
+
+var greet = (name) => {
+    console.log("Merhaba, " + name + "!");
+  };
+  
+  // Fonksiyonu çağırma
+  greet("Mehmet"); // Çıktı: Merhaba, Mehmet!
+
+//!4- Function Constructor (Fonksiyon Oluşturucu):
+
+var myFunction = new Function('a', 'b', 'return a + b');
+
+
+var greet = new Function('name', 'console.log("Merhaba, " + name + "!");');
+
+// Fonksiyonu çağırma
+greet("Fatma"); // Çıktı: Merhaba, Fatma!
+
+  
