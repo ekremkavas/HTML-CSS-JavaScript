4a088b224bf14ce257c96285a821e5433c42d071
ekrem <ekremkavas2012@gmail.com>
2024-01-31 14:25:35 +0300

    run

--- RunJavascript/js04-Loops/04_step1for.js
+++ RunJavascript/js04-Loops/04_step1for.js
@@ -6,11 +6,37 @@
 
 
 // *************5 e kadar yazdırma***********
-  for (let i = 1; i <= 5; i++) {
-    console.log(i);
-  }
+//   for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+//   }
 
+// *************çift sayıları yazdırma**********
+// for (let i = 0; i <= 10; i += 2) {
+//     console.log(i);
+//   }
+  
 // ******************************************
 
-  
-  
+// for(let i = 1; i <= 10; i++){
+//     if(i == 3){
+//     console.log('en sevdiğim rakam :'+i);
+//     continue;
+//     }
+//     if(i == 6){
+//         console.log('en sevdiğim rakam :'+i);
+//         break;
+//     }
+
+//     console.log(i);|
+// }
+
+// *************************************
+// console.log("pratik isim yazdırma");
+// for(let i = 1; i <= 7 ; i++){
+//     if(i % 2 == 1){
+//         console.log("Ekrem");
+//     }else{
+//         console.log("Cenkeri");
+//     }
+// }
+    // ****************************
