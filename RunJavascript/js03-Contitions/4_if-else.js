f7b39d25bce1d646c75cece9fdd6827d216d116e
ekrem <ekremkavas2012@gmail.com>
2024-01-31 11:44:34 +0300

    run

--- RunJavascript/js03-Contitions/4_if-else.js
+++ RunJavascript/js03-Contitions/4_if-else.js
@@ -1,23 +1,139 @@
 //KOŞUL YAPILARI
 
-let not = 65;
-if (not > 50) {
-  console.log("if bloğunun içi true çalıştı");
-} else {
-  console.log("if bloğu false else bloğu çalıştı");
-}
+// let not = 65;
+// if (not > 50) {
+//   console.log("if bloğunun içi true çalıştı");
+// } else {
+//   console.log("if bloğu false else bloğu çalıştı");
+// }
 
-let not2 = 35;
-if (not2 > 50) {
-  console.log("if bloğunun içi false çalışmadı");
-} else {
-  console.log("if bloğu false else bloğu çalıştı");
-}
+// let not2 = 35;
+// if (not2 > 50) {
+//   console.log("if bloğunun içi false çalışmadı");
+// } else {
+//   console.log("if bloğu false else bloğu çalıştı");
+// }
 
-sayi = Number(prompt("bir sayı giriniz"))
-if(sayı % 2 0= 0){
-  console.log("Çift sayı girdiniz");
+// *************TEK-ÇİFT SORGULAMA**************
+
+// sayi = Number(prompt("bir sayı giriniz"));
+// if (sayi == 0 || sayi < 0){
+//   console.log("Tekrar deneyin 0 veya pozitif bir sayı giriniz", sayi);
+
+//   sayi = Number(prompt(" Tekrar sıfır harici bir sayı giriniz veya pozitif bir sayı giriniz"));
+
+//   if (sayi % 2 == 0){
+//     console.log("çift sayı girdiniz");
+//   }else{
+//     console.log("tek sayı girdiniz", sayi);
+//   }
+// }else if (sayi % 2 == 0) {
+//   console.log("çift sayı girdiniz");
+// }else {
+//   console.log("tek sayı girdiniz", sayi);
+// }
+
+// *************1-İLA 7 ARASINDA BİR SAYI**********************
+
+// sayi2 = Number(prompt("1-7 arasında 1 sayı giriniz"));
+// if (sayi2 <= 7 && sayi2 >= 1) {
+//   console.log("istenilen sayiyi girdiniz :", sayi2);
+// } else if (sayi2 > 7 || sayi2 < 1) {
+//   alert("istenilen sayı değildir yeniden giriniz");
+//   sayi2 = Number(prompt("1-7 arasında bir sayı giriniz"));
+//   sayi2 = sayi2 <= 7 && sayi2 >= 1  //ternary kullandım
+//       ? console.log("istenilen sayıyı girdiniz")
+//       : console.log("yanlış sayıyı girdiniz");
+// }
+
+// ********************************************************
+//if else pratik 2
+// gun = Number(prompt("1-7 arasında bir sayı giriniz"));
+// if(gun <= 7 && gun >= 1) {
+// switch (gun) {
+//   case 1:
+//     console.log("Pazartesi");
+//     break;
+//   case 2:
+//     console.log("Salı");
+//     break;
+//   case 3:
+//     console.log("Çarşamba");
+//     break;3
+//   case 4:
+//     console.log("Perşembe");
+//     break;
+//   case 5:
+//     console.log("Cuma");
+//     break;
+//   case 6:
+//     console.log("Cumartesi");
+//     break;
+//   case 7:
+//     console.log("Pazar");
+//     break;
+//   default:
+//     console.log("Geçersiz gün numarası!");
+// }
+// }else if (gun > 7 || gun < 1) {
+//   alert("istenilen sayı değildir yeniden giriniz");
+// }
+
+// *************EHLİYET SORGULAMA******************************
+
+// //Pratik-3
+// let yas = Number(prompt("Lütfen yaşınızı giriniz"))
+// let ehliyet = prompt("Ehliyet durumunuzu mevcut ise '+' yok ise '-'şeklinde belirtiniz")
+// if(yas >= 18 && ehliyet == "+"){
+//   console.log("Araç kullanabilir");
+// }else{
+//   console.log("Araç kullanamaz");
+// }
+
+// *****************KULLANICIDAN ID-PASWORLD ISTEME***********
+
+// //Pratik 4
+// let ID = prompt("Lütfen kullanıcı adınızı giriniz:");
+// let password = prompt("Lütfen şifrenizi giriniz:");
+
+// //Sistemde kayıtlı id ve password bu olsun
+
+// let kayitli_ID = "Clarusway"
+// let kayitli_password = "fullstack16"
+
+// if(ID == kayitli_ID && password == kayitli_password){
+//   console.log("Clarusway kariyer yolculuğuna hoş geldiniz");
+// }else if(ID == kayitli_ID){
+//   console.log("şifrenizi kontrol ediniz");
+// }else if(password == kayitli_password){
+//   console.log("ID adresiniz hatalıdır");
+// }else{
+//   console.log("Tekrar deneyiniz");
+// }
+
+// *************4 İŞLEM*******************
+
+let değer1 = Number(prompt("Lütfen bir sayı giriniz"));
+let değer2 = Number(prompt("Lütfen bir sayı giriniz"));
+let islem = prompt("Lütfen yapmak istediğiniz işlemi seçiniz");
+
+if (true) {
+  switch (islem) {
+    case "+":
+      console.log(değer1 + değer2);
+      break;
+    case "-":
+      console.log(değer1 - değer2);
+      break;
+    case "*":
+      console.log(değer1 * değer2);
+      break;
+    case "/":
+      console.log(değer1 / değer2);
+      break;
+    default:
+      console.log("Geçerli değerleri giriniz");
+  }
 }else{
-  console.log("tek sayı girdiniz");
+  alert("istenilen değerleri giriniz")
 }
-   
