a16e08f591ba995ec788808e4c6fe5a9313c18e1
Ekrem <ekremkavas2012@gmail.com>
2024-02-01 05:09:23 +0300

    run

--- RunJavascript/js05-function/050_function.js
+++ RunJavascript/js05-function/050_function.js
@@ -1,77 +1,60 @@
-
-//! 1-Function Decleration 
+//! 1-Function Decleration
 
 // Function Declaration (Fonksiyon Deklarasyonu):ile Tanımlama'da fomksiyonun yerinin önemi yok nereye yazarsanız orda çağırır
 
 function myFunction() {
-    // Fonksiyonun işlemleri burada tanımlanır
-  }
-  
+  // Fonksiyonun işlemleri burada tanımlanır
+}
 
-function Cenkeri(){
-    console.log("Ben 5 yaşındayım");
+function Cenkeri() {
+  console.log("Ben 5 yaşındayım");
 }
 //invoke (call)
-Cenkeri(); 
-
+Cenkeri();
 
 function greet(name) {
-    console.log("Merhaba, " + name + "!");
-  }
-  
-  // Fonksiyonu çağırma
-  greet("Ahmet"); // Çıktı: Merhaba, Ahmet!
-
-  
-//! 2-Function Expression (Fonksiyon İfadesi): 
-
-var myFunction = function() {
-    // Fonksiyonun işlemleri burada tanımlanır
-  };
-  
-
-var greet = function(name) {
-    console.log("Merhaba, " + name + "!");
-  };
-  
-  // Fonksiyonu çağırma
-  greet("Ayşe"); // Çıktı: Merhaba, Ayşe!
-  
-
-function selamVer(mesaj){
-    console.log(`Merhaba ${mesaj}`);
-}
-selamVer("Esat")
-selamVer("Ekrem")
-
-function topla(s1, s2){
-    console.log("toplam :", s1 + s2);
+  console.log("Merhaba, " + name + "!");
 }
 
-topla(7,5)
+// Fonksiyonu çağırma
+greet("Ahmet"); // Çıktı: Merhaba, Ahmet!
+
+//! 2-Function Expression (Fonksiyon İfadesi):
+
+var myFunction = function () {
+  // Fonksiyonun işlemleri burada tanımlanır
+};
+
+var greet = function (name) {
+  console.log("Merhaba, " + name + "!");
+};
+
+// Fonksiyonu çağırma
+greet("Özlem"); // Çıktı: Merhaba, Özlem!
+
+function selamVer(mesaj) {
+  console.log(`Merhaba ${mesaj}`);
+}
+selamVer("Esat");
+selamVer("Ekrem");
+
+function topla(s1, s2) {
+  console.log("toplam :", s1 + s2);
+}
+
+topla(7, 5);
 
 //!3-Arrow Function (Ok Fonksiyonu):
 
 var myFunction = () => {
-    // Fonksiyonun işlemleri burada tanımlanır
-  };
-  
+  // Fonksiyonun işlemleri burada tanımlanır
+};
 
-var greet = (name) => {
-    console.log("Merhaba, " + name + "!");
-  };
-  
-  // Fonksiyonu çağırma
-  greet("Mehmet"); // Çıktı: Merhaba, Mehmet!
+// süslü parantez kullanırsanız return kullanmanız lazım
+const kareAliver = (x) => {
+  return x * x;
+};
+console.log(kareAliver(5));
 
-//!4- Function Constructor (Fonksiyon Oluşturucu):
-
-var myFunction = new Function('a', 'b', 'return a + b');
-
-
-var greet = new Function('name', 'console.log("Merhaba, " + name + "!");');
-
-// Fonksiyonu çağırma
-greet("Fatma"); // Çıktı: Merhaba, Fatma!
-
-  
+const kareAl = (x) => x * x;
+console.log(kareAl(7));
