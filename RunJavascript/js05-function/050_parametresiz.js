412277b58089629ace91051f86fcb059bb7b5f24
Ekrem <ekremkavas2012@gmail.com>
2024-02-01 07:39:37 +0300

    run

---
+++ RunJavascript/js05-function/050_parametresiz.js
@@ -0,0 +1,21 @@
+//! PARAMETRESİZ  VE DEĞER DÖNDÜRMEYEN METOT TANIMLAMAK
+//! 1-Function Decleration
+
+//BASİC METOD TANIMI
+
+function yazdir(){
+    //? yazdırın içi paremetre kısmı burası
+    //kodları buraya yaz tanımlamak ıstedıgın
+  
+    console.log("Cenkeri"); // metodu hazırladık consola cıktı vermez 
+}
+yazdir(); // tanımladıgım metodu calıstırmak ıstıyorum
+
+yazdir(); 
+
+
+function mod() {
+    console.log(10 % 4);
+}
+
+mod();
---
+++ RunJavascript/js05-function/052_return.js
@@ -0,0 +1,22 @@
+//! RETURN -GERİYE DEĞER DÖNDÜREN FONKISYON
+
+
+//? 1-return bır değeri metodun dısırısına cıkarmak ve tasımak ıcın kullanılır
+//? 2-return bır metodu bıtıren anahtar kelımedır sonra gelen kodu yazdırmaz
+function usAl(rakam1, rakam2) {
+  let sonuc = rakam1 ** rakam2;
+  console.log(sonuc); // clh metodun ıcınde kaldı
+  // return yazmazsak sonuc u cagırsan bıle fonkısyon gelmez
+}
+
+usAl(5, 2);
+
+function kareAl(değer) {
+  let sonuc = değer * değer;
+  return sonuc; // return  döndürdüğü değer metodun çağırdığı yere gider
+  // ek bilgi return anahtarkelımesınden sonra yazmıs oldugunuz hıcbır kod calısmaz
+}
+
+console.log(kareAl(6));
+
+
