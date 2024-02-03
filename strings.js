82b126d6b1dd6fdcb74212ddc2218ece204ffcd6
ekrem <ekremkavas2012@gmail.com>
2024-02-03 11:30:30 +0300

    run

---
+++ strings.js
@@ -0,0 +1,21 @@
+/****************STRİNGS******************** */
+deger = "F-string gibi backtick"
+//? String oluşturma yöntemleri
+let str1 = 'Bu bir dize';
+let str2 = "Bu da bir dize";
+let str3 = `Bu da bir dize ${deger}`;
+let str4 = new String("new string ")
+console.log(str1);
+console.log(str2);
+console.log(str3);
+console.log(str4);
+console.log("🎂❤🎁");
+
+
+/*******ESCAPE KARAKTER(TERS SLAŞH)***** */
+console.log("Merhaba\nDünya");   // Yeni satıra geçer
+console.log("Bir\tiki\tüç");     // Sekme boşluğu ekler
+console.log("Bir\biki");         // Bir karakter geriye gider
+console.log('Tek tırnak: \' ');  // Tek tırnak kullanımı
+console.log("Cift tırnak: \" "); // Çift tırnak kullanımı
+console.log("Ters eğik çizgi: \\ "); // Ters eğik çizgi kullanımı
