7713ca5fef2abe670e7fb8b12cd00428b260b758
Ekrem <ekremkavas2012@gmail.com>
2024-02-08 20:38:02 +0300

    run

---
+++ RunJavascript/js09-ArrayFor/09_arrayFor.js
@@ -0,0 +1,12 @@
+var numbers = [1, 2, 3, 4, 5];
+for (var i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
+
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        console.log(numbers[i] + " çift sayıdır.");
+    }
+}
