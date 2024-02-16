6aae93998cf84c973273efa77971309194026d80
ekrem <ekremkavas2012@gmail.com>
2024-02-16 11:52:34 +0300

    run

--- RunJavascript/js09-ArrayFor/others.js
+++ RunJavascript/js09-ArrayFor/others.js
@@ -40,18 +40,37 @@ function isPrime(num) {
 //   Bu örnekte, isPrime() fonksiyonu her bir sayının asal olup olmadığını kontrol eder ve every() yöntemi tüm sayıların asal olup olmadığını kontrol eder. Tüm sayılar asal olduğu için true döner.
 
 
+//*---------------------------
+//*          some()
+//*---------------------------
+//? Aldigi callback fonksiyonuna gore test gerceklestirir.En az bir eleman icin bile test basarili ise true aksi takdirde false deger dondurur.
 
-//* some()
-//*-----------------------------------------------------------
-//? Aldigi callback fonksiyonuna gore test gerceklestirir.
-//? En az bir eleman icin bile test basarili ise true aksi
-//? takdirde false deger dondurur.
+// En Az Bir Kelime Belirli Bir Uzunlukta mı?
+
+const woords = ["apple", "banana", "orange", "grape"];
+const hasFiveLetters = woords.some(word => word.length === 5);
+console.log(hasFiveLetters); // true
 
 const over80 = ages.some((a) => a >= 80);
 console.log(over80); //? true
 
+//*--------------------------------
 //* find(), findLast()
-//*-----------------------------------------------------------
+//--------------------------------------
+
+// find() yöntemi, bir dizi içinde belirli bir koşulu karşılayan ilk öğeyi bulmak için kullanılır. find() yöntemi, belirtilen koşulu sağlayan ilk öğeyi bulduğunda aramayı durdurur ve bu öğeyi döndürür. Eğer koşulu sağlayan öğe bulunamazsa undefined değeri döner.
+const names = ["Alice", "Bob", "Charlie", "David"];
+const foundName = names.find(name => name === "Charlie");
+console.log(foundName); // Charlie
+
+// Bu fonksiyon, bir dizide belirli bir koşulu sağlayan son öğeyi bulmak için kullanılır. findLast fonksiyonu, find fonksiyonunun tersine çalışır; yani dizide aramaya sondan başlar.
+
+const nnnumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const lastEven = findLast(nnnumbers, num => num % 2 === 0);
+console.log(lastEven); // 10
+
+
+
 //? Aldigi callback fonksiyonuna gore test gerceklestirir.
 //? Kosulu saglayan ilk dizi elemaninin dondurur.
 //? Eger hic bir eleman kosulu saglamazsa undefined dondurur.
@@ -63,8 +82,16 @@ console.log(ageBigThan30First, ageBigThan30Last);
 
 console.log(ages.find((age) => age >= 80)); //? undefined
 
+//*--------------------------------
 //* findIndex()
-//*-----------------------------------------------------------
+//*--------------------------------
+
+// findIndex() yöntemi, bir dizide belirli bir koşulu sağlayan ilk öğenin dizideki indeksini bulmak için kullanılır. Bu yöntem, find() yöntemi gibi çalışır, ancak bulunan öğenin indeksini döndürür, bulunamazsa -1 değerini döndürür.
+
+const numbery = [10, 20, 30, 40, 50];
+const index = numbery.findIndex(num => num === 30);
+console.log(index); // 2
+
 //? Aldigi callback fonksiyonuna gore test gerceklestirir.
 //? Kosulu saglayan ilk dizi elemaninin indeksini dondurur.
 //? Eger hic bir eleman kosulu saglamazsa -1 dondurur.
