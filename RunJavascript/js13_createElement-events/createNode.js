d1fcbdb387dc6ae5f66eea212baa1ad4018cbe14
ekrem <ekremkavas2012@gmail.com>
2024-02-28 18:16:12 +0300

    run

--- RunJavascript/js13_createElement-events/createNode.js
+++ RunJavascript/js13_createElement-events/createNode.js
@@ -6,44 +6,47 @@ console.log("**** CREATE NODE ****")
 
 // 1- innerhtml --> az güvenlikli
 
-// 2.Sİ AŞAĞĞIDAKİ DÖRT YÖNTEM:
+// 2.Sİ AŞAĞIDAKİ DÖRT YÖNTEM:
 
 
 
 //! 1- yeni bir li elementi olustur
 const newLi = document.createElement("li")
-//**burda yeni li elemnti oluşturduk ama içeriği boş ! hiç bir yere bağlı değilk yani doma bağlı değil bağımsız.içeriğinde bilgi de yok . şimdi buna bir de text oluşturalım:
+//**burda yeni li elemnti oluşturduk ama içeriği boş, hiç bir yere bağlı değil yani doma bağlı değil bağımsız.içeriğinde bilgi de yok . şimdi buna bir de text oluşturalım:
 //document.createElement("html elemnti ismi mesala --> "div" dersek bu bize bir div oluşturr, "a","section" vs. bunları belirterek istediğimiz elementi oluşturabiliriz.)--> domda tanımlanmış methot .bir element oluşturabiliyoruz.
 // ama bu yeterli değil. o element oluştuktan sonra o elemnt bir yazı elemntiyse h1 gibi p gibi div section gibi yani içersinde textnode olan (first child)
 
+// console.log(newLi);
 
 //! 2- Eger text tabanlı bir element ise text dugumunu olustur
 const textLi = document.createTextNode("Go")
 
 //*içerik oluşturdk ama hala göremiyoruz. çünkü hala bir yere bağlı değil.
-// document.createTextNode(text) -->yani ikinci adımda ilgili element için bir textnode meydana getirmek gerejkiyor. bu da yeterli değil
+// bu textNode newli ye bağlammaız lazım bunun için 3.adaım appendChild yapacağız
 
 //! 3- Olusturulan text dugumunu yeni elemente bagla (append)
 newLi.appendChild(textLi)
-//*OLUŞTU FAKAT DOMA BAĞLI DEĞİL. DOM DA NEREYE BAĞLAYACKSAK ORAYA append ETMELİYİZ.
-// 3- document.appendChild(text düğüm) --> oluşturduğumuz bu text nodu oluşturduğumuz textnoda bağlamamız gerek.
+//*oluştu içerik ama bu Tree(DOM) ile deilişkindirilmesi lazım ona da bağlayalım
+
+// console.log(newLi);
 
 
 //!4- Yeni olusan elementi DOM tree'ye ekle.
 document.querySelector("ul").appendChild(newLi)
-// 4- element.removeChild(child düğüm)--> oluşturduğumuz yeni elemnti de domas bağlamamız gerekiyor.
 
 // document.querySelector("body").appendChild(newLi) // eklendi consoleda elemnt kısmına baktığımızda eklendiğini gördük ama en son akış  neredeyse oraya ekledi body seçtiğimniz için.
-//  UL'NİN SONUNA EKLMEK İSTİYORUZ. O YÜZDEN BODY YERİNE UL YAZACAĞIZ.
+
+//  UL'NİN SONUNA EKLMEK İSTİYORUZ. O YÜZDEN BODY YERİNE UL YAZACAĞIZ.ul yazan yerde body vardı , öyle olunca yalnıs tree e ekledı
 // artık ulnin en sonunda go görmüş oluyoruz.
 
 
-
+// ************************************************
 
 //?**** 2. bir li olusturalım
 
 const newLi2 = document.createElement("li")
 newLi2.textContent = "C++"
+// console.log(newLi2);
 
 // ! MDN den baktık: element.before()  veya elemnet.after() bu şekilde önüne ve sonuna istediğimiz elemnti veya virgülle istediğimniz kadar element ekleyebiliriz.
 
@@ -54,11 +57,12 @@ newLi2.textContent = "C++"
 const react = document.querySelector("ul li:nth-child(4)")
 react.before(newLi2, newLi)
 
+// ****************************************************
 
-
-//**-- ID, CLASS GIBI OZELLIKLERI ATAMA ---
+//!-- ID, CLASS GIBI OZELLIKLERI ATAMA ---
 
 //! 1- element.attribute = "yeni deger"
+
 //* yazma
 //burda da birden fazla class verebiliriz.
 newLi.className = "list bold text-success bg-warning"
@@ -69,10 +73,13 @@ newLi.name = "list"
 console.log(newLi.className)
 
 //! 2- setAttribute(atr,"deger") getAttrbute("atr")
+
+// setAttribute yazar getAttribute okur
 newLi2.setAttribute("class", "list red bold")
 
 console.log(newLi2.getAttribute("id"))
 console.log(newLi2.getAttribute("class"))
+
 
 //! 3- Yontem (classList)
 newLi.classList.add("border", "border-danger", "border-3")
