62697e54f466d282ad426267f7a2102827a8aec2
ekrem <ekremkavas2012@gmail.com>
2024-03-06 18:13:33 +0300

    run

--- RunJavascript/js13_createElement-events/13x1_create.js
+++ RunJavascript/js13_createElement-events/13x1_create.js
@@ -7,3 +7,6 @@ const textEkle = document.createTextNode("anlar")
 newEkle.appendChild(textEkle)
 //selecktor ıle secıp yenı olusan elementı DOM tree ye eklemıs olduk
 document.querySelector("ul").appendChild(newEkle)
+
+newEkle.className = "list"
+newEkle.id = "new-li"
