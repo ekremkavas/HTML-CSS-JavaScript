0bf87f68e659af13942d70a33c342dd1bb81054b
Ekrem <ekremkavas2012@gmail.com>
2024-03-07 20:25:48 +0300

    run

---
+++ JavaScript-CLASS/js17_async1/3-fetchAPI.js
@@ -0,0 +1,39 @@
+//*========================================
+//*             3 - FETCH API
+//*========================================
+
+//? Dis kaynaklardan veri getirmek icin kullanilan basit bir arabirimdir.
+//? Ag istekleri yapmamizi ve cevaplari yontebilmemize olanak saglar.
+//? Javascript ortaminda en cok kullanilan Asenkron islem orneklerinin basinda
+//? gelmektedir.
+
+//? fetch() fonksiyonu veri getirmek istediginiz kaynagin yolunu gosteren zorunlu
+//? bir parametre almaktadir ve bu istegin cevabini gosteren bir Promise dondurmektedir.
+
+console.log("FETCH")
+
+// let userData = ""
+fetch("https://api.github.com/users")
+  .then((res) => {
+    console.log(res)
+    //? Fetch api'da hatayi bizim yakalamiz gerekiyor.
+    if (!res.ok) {
+      throw new Error(`Something went wrong ${res.status}`)
+    }
+    return res.json()
+  })
+  .then((data) => {
+    // console.log(data)
+    // userData = data
+    // console.log(userData)
+    showUser(data)
+  })
+  .catch((err) => document.write(err))
+
+// console.log(userData)
+
+const showUser = (data) => {
+  console.log(data)
+  const userSection = document.getElementById("users")
+// userSection.innerHTML += 
+}
