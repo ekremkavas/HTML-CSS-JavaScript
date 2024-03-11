d7a31dc6a29b868be89477a45ab61fb8da02c610
ekrem <ekremkavas2012@gmail.com>
2024-03-11 18:31:12 +0300

    run

--- RunJavascript/js13_createElement-events/13x3_minicalisma.js
+++ RunJavascript/js13_createElement-events/13x3_minicalisma.js
@@ -8,9 +8,11 @@ console.log(getBtn);
 getBtn.addEventListener("click", () => {
     //buton uzerıne geldıgınde dınleme yapıldı
     console.log("butona tıklandı");
-    const inputdegeri = document.querySelector("#searchText").value;
+    const inputdegeri = document.querySelector("#searchText").value.trim();
     //ınput ıcergı yakalandı
+    //VALUE SONUNDAKI TRIM METHODU KULLANICI BASINDA SONUNDA BOSLUK GIRMESIN DIYE
     console.log("ınput ıcıne yazılan değer  = ",inputdegeri);
     //consolda hep kontrol et aşama aşama yakaladın mı dıye
     //ınputa bırseyler yazıp tıklandı console ıle kontrol edıldı
+
 })
